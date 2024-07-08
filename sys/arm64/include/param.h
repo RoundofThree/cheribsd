35113f58e74490016f58b064db14cc36de43958a
Yichen Chai <34469805+YiChenChai@users.noreply.github.com>
2024-07-08 23:33:06 +0100

    Increase kernel stack pages when coverage is enabled

--- sys/arm64/include/param.h
+++ sys/arm64/include/param.h
@@ -113,7 +113,9 @@
 #define	MAXPAGESIZES	3		/* maximum number of supported page sizes */
 
 #ifndef KSTACK_PAGES
-#ifdef __CHERI_PURE_CAPABILITY__
+#ifdef COVERAGE
+#define KSTACK_PAGES    6
+#elif defined(__CHERI_PURE_CAPABILITY__)
 #define	KSTACK_PAGES	5	/* pages of kernel stack (with pcb) */
 #elif defined(KASAN) || defined(KMSAN)
 #define	KSTACK_PAGES	6
