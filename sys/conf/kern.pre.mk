7c3793fdb1ea513e8f4b313461156a13d0b8d975
RoundofThree <zhuoyingjiangli@gmail.com>
2024-08-04 21:55:07 +0100

    syzkaller: Enable selective sanitization for CHERI purecap kernel
    Disable sanitization flags that overlap with CHERI (eg. dynamic allocas can be caught by capability bounds set by CheriBoundsAlloca module pass in CHERI LLVM).

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -93,6 +93,17 @@ COMPAT_FREEBSD32_ENABLED!= grep COMPAT_FREEBSD32 opt_global.h || true ; echo
 
 KASAN_ENABLED!=	grep KASAN opt_global.h || true ; echo
 .if !empty(KASAN_ENABLED)
+.if ${MACHINE_ABI:Mpurecap}
+SAN_CFLAGS+=	-DSAN_NEEDS_INTERCEPTORS -DSAN_INTERCEPTOR_PREFIX=kasan \
+		-fsanitize=kernel-address \
+		-mllvm -asan-stack=false \
+		-mllvm -asan-instrument-dynamic-allocas=false \
+		-mllvm -asan-globals=false \
+		-mllvm -asan-use-after-scope=true \
+		-mllvm -asan-instrumentation-with-call-threshold=0 \
+		-mllvm -asan-instrument-byval=false \
+		-mllvm -asan-use-after-return=always
+.else
 SAN_CFLAGS+=	-DSAN_NEEDS_INTERCEPTORS -DSAN_INTERCEPTOR_PREFIX=kasan \
 		-fsanitize=kernel-address \
 		-mllvm -asan-stack=true \
@@ -101,7 +112,7 @@ SAN_CFLAGS+=	-DSAN_NEEDS_INTERCEPTORS -DSAN_INTERCEPTOR_PREFIX=kasan \
 		-mllvm -asan-use-after-scope=true \
 		-mllvm -asan-instrumentation-with-call-threshold=0 \
 		-mllvm -asan-instrument-byval=false
-
+.endif 
 .if ${MACHINE_CPUARCH} == "aarch64"
 # KASAN/ARM64 TODO: -asan-mapping-offset is calculated from:
 #	   (VM_KERNEL_MIN_ADDRESS >> KASAN_SHADOW_SCALE_SHIFT) + $offset = KASAN_MIN_ADDRESS
